ca5ca4031aa8f7596b6923d6a61ee0316c1841e8
jellyfina <88398027+jellyfina@users.noreply.github.com>
2021-08-11 23:22:07 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -80,7 +80,7 @@ rm -rf ./package/lean/vsftpd-alt
 rm -rf ./package/lean/luci-app-vsftpd
 rm -rf ./feeds/luci/applications/luci-app-adguardhome
 git clone https://github.com/skycn3322/luci-app-adguardhome.git package/adguardhome
-git clone https://github.com/kiddin9/luci-theme-edge/branches/18.06 package/luci-theme-edge
+git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 # 自定义插件
 sed -i '$a src-git serverchan https://github.com/tty228/luci-app-serverchan' feeds.conf.defaul
 
